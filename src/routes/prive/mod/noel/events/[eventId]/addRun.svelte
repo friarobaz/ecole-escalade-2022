73d6724da862dfdd8517a73d35780df9280f1d58
friarobaz <friarobaz@gmail.com>
2023-11-24 00:36:00 +0100

    add form to add other students not from ee

--- src/routes/prive/mod/noel/events/[eventId]/addRun.svelte
+++ src/routes/prive/mod/noel/events/[eventId]/addRun.svelte
@@ -64,7 +64,7 @@
             </hgroup>
             {#if !selectedRoute}
                 <!-- <Routes routes={studentCategory(selectedStudent, categories).data().routes} bind:selectedRoute={selectedRoute}/> -->
-                <Lines routes={studentCategory(selectedStudent, categories).data().routes} bind:selectedRoute={selectedRoute}/>
+                <Lines routes={studentCategory(selectedStudent, categories).data().routes} bind:selectedRoute={selectedRoute} showMulticolor={false} />
             {:else}
                 
                 <RunConfirmation route={selectedRoute} student={selectedStudent} {event} category={studentCategory(selectedStudent, categories)} bind:reset={reset}/>
